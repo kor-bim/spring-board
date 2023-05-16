559d4f1f443e50a0dcef3f9be777681d7abdcae9
kor-bim <dbsgksqlschl@gmail.com>
2023-05-16 20:09:09 +0900

    feat(#5) - 게시판 뷰 기본 골격

--- src/test/java/com/bim93/springboard/controller/ArticleControllerTest.java
+++ src/test/java/com/bim93/springboard/controller/ArticleControllerTest.java
@@ -21,13 +21,12 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("개발중")
     @DisplayName("[view][GET] article page - success request")
     @Test
     void articles() throws Exception {
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
     }
@@ -38,7 +37,7 @@ class ArticleControllerTest {
     void articlesDetail() throws Exception {
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
@@ -50,7 +49,7 @@ class ArticleControllerTest {
     void articlesSearch() throws Exception {
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/search"));
     }
 
@@ -60,7 +59,7 @@ class ArticleControllerTest {
     void articlesHashtagSearch() throws Exception {
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/search-hashtag"));
     }
 }
